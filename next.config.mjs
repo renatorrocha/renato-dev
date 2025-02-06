99b84c9998a739c39c42b3f67539a02ebc6d33ff
renatorrocha <renatorrodrigues2002@gmail.com>
2025-02-06 17:45:05 -0300

    feat: add permanent redirect from root to personal website (SEO issues)

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/services/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://renatodev.com/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
