63bb9dc25d62d2b86a753fcbc47731e8c38e82d4
renatorrocha <renatorrodrigues2002@gmail.com>
2025-04-16 18:59:20 -0300

    refactor: enhance blog layout and post page with improved styling and navigation

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -3,6 +3,10 @@ import BlurFadeText from "@/components/blur-fade-text";
 import { BLUR_FADE_DELAY } from "@/lib/constants";
 import { getPost } from "@/services/blog/get-post";
 import Markdown from "react-markdown";
+import { Button } from "@/components/ui/button";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import { Link } from "@/services/i18n/navigation";
+import { getTranslations } from "next-intl/server";
 
 export default async function BlogPostPage({
   params,
@@ -10,36 +14,51 @@ export default async function BlogPostPage({
   params: { slug: string; locale: string };
 }) {
   const post = await getPost(params.locale, params.slug);
+  const t = await getTranslations("Blog");
 
-  console.log(post);
+  if (!post) {
+    return <div>Post not found</div>;
+  }
 
   return (
     <section id="blog">
-      <div className="mx-auto w-full max-w-2xl space-y-8">
-        <div className="flex flex-col">
-          <BlurFadeText
-            delay={BLUR_FADE_DELAY}
-            className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
-            yOffset={8}
-            text={params.slug}
-          />
+      <BlurFade delay={BLUR_FADE_DELAY * 2}>
+        <Link href={"/blog"}>
+          <Button
+            variant="ghost"
+            className="group flex items-center gap-2 text-secondary hover:text-primary"
+          >
+            <ArrowLeftIcon className="h-4 w-4 transition-transform group-hover:-translate-x-1" />
+            <span>{t("backToBlog")}</span>
+          </Button>
+        </Link>
+      </BlurFade>
 
+      <div className="flex flex-col">
+        <BlurFadeText
+          delay={BLUR_FADE_DELAY}
+          className="text-lg font-bold tracking-tighter sm:text-2xl xl:text-3xl/none"
+          yOffset={8}
+          text={post?.meta.title}
+        />
+
+        {post?.meta.description && (
           <BlurFadeText
             className="max-w-[600px] text-secondary md:text-xl"
             delay={BLUR_FADE_DELAY}
-            text={params.slug}
+            text={post?.meta.description}
           />
+        )}
 
-          <BlurFade
-            key={post?.meta.title}
-            delay={BLUR_FADE_DELAY * 8}
-            className="mt-4"
-          >
-            <Markdown className="prose dark:prose-invert max-w-full text-pretty font-sans text-sm text-secondary">
-              {post?.content}
-            </Markdown>
-          </BlurFade>
-        </div>
+        <BlurFade
+          key={post?.meta.title}
+          delay={BLUR_FADE_DELAY * 8}
+          className="mt-4"
+        >
+          <Markdown className="prose dark:prose-invert max-w-full text-pretty rounded-md border bg-primary-foreground p-2 font-sans text-sm text-secondary">
+            {post?.content}
+          </Markdown>
+        </BlurFade>
       </div>
     </section>
   );
--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -14,47 +14,45 @@ export default async function BlogPage({
   const t = await getTranslations("Blog");
 
   return (
-    <section id="blog">
-      <div className="mx-auto w-full max-w-2xl space-y-8">
-        <div className="flex flex-col gap-4">
-          <BlurFadeText
-            delay={BLUR_FADE_DELAY}
-            className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
-            yOffset={8}
-            text={t("title")}
-          />
+    <section id="blog" className="space-y-8">
+      <div className="flex flex-col gap-4">
+        <BlurFadeText
+          delay={BLUR_FADE_DELAY}
+          className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+          yOffset={8}
+          text={t("title")}
+        />
 
-          <BlurFadeText
-            className="max-w-[600px] text-secondary md:text-xl"
-            delay={BLUR_FADE_DELAY}
-            text={t("description")}
-          />
-        </div>
+        <BlurFadeText
+          className="max-w-[600px] text-secondary md:text-xl"
+          delay={BLUR_FADE_DELAY}
+          text={t("description")}
+        />
+      </div>
 
-        <div className="flex flex-col gap-4">
-          {posts.length > 0 ? (
-            posts.map((post, idx) => (
-              <BlurFade
-                key={post?.meta.title}
-                delay={BLUR_FADE_DELAY * 8 + idx * 0.05}
-              >
-                <BlogCard
-                  key={post?.slug}
-                  title={post?.meta.title ?? ""}
-                  description={post?.meta.description ?? ""}
-                  date={new Date(post?.meta.date ?? "")}
-                  href={`/blog/${post?.slug}`}
-                />
-              </BlurFade>
-            ))
-          ) : (
-            <BlurFadeText
-              className="mx-auto text-center text-muted-foreground"
-              delay={BLUR_FADE_DELAY + 1}
-              text={t("noPosts")}
-            />
-          )}
-        </div>
+      <div className="flex flex-col gap-4">
+        {posts.length > 0 ? (
+          posts.map((post, idx) => (
+            <BlurFade
+              key={post?.meta.title}
+              delay={BLUR_FADE_DELAY * 8 + idx * 0.05}
+            >
+              <BlogCard
+                key={post?.slug}
+                title={post?.meta.title ?? ""}
+                description={post?.meta.description ?? ""}
+                date={new Date(post?.meta.date ?? "")}
+                href={`/blog/${post?.slug}`}
+              />
+            </BlurFade>
+          ))
+        ) : (
+          <BlurFadeText
+            className="mx-auto text-center text-muted-foreground"
+            delay={BLUR_FADE_DELAY + 1}
+            text={t("noPosts")}
+          />
+        )}
       </div>
     </section>
   );
